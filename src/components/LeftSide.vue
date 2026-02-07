4312318a9de37558be120a33ae16613a5e0091e6
YuyaItabashi3594 <asuholover@gmail.com>
2026-02-07 11:50:21 +0900

    init

---
+++ src/components/LeftSide.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>test</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+h1,
+h3 {
+  text-align: center;
+}
+</style>
